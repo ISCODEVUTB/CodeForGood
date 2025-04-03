8e99a2c03ff89e9004f732b028714a5c146f0cb1
guemibachata <miguevd8@gmail.com>
2025-04-03 13:30:25 -0500

    Update models.py

--- Services/models.py
+++ Services/models.py
@@ -3,7 +3,7 @@ from Services.database import Base
 
 class Donor(Base):
     __tablename__ = "donors"
-    __table_args__ = {"extend_existing": True}  # AGREGAR ESTO
+    __table_args__ = {"extend_existing": True}  
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, nullable=False)
     email = Column(String, unique=True, nullable=False)
@@ -11,7 +11,7 @@ class Donor(Base):
 
 class Volunteer(Base):
     __tablename__ = "volunteers"
-    __table_args__ = {"extend_existing": True}  # AGREGAR ESTO
+    __table_args__ = {"extend_existing": True}  
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, nullable=False)
     phone = Column(String, unique=True, nullable=False)
